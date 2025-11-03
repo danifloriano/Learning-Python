1066c0363a929ffec16c99e998394742a5ab8873
danifloriano <118399363+danifloriano@users.noreply.github.com>
2025-11-03 19:50:19 -0300

    Add files via upload

---
+++ mod_func.py
@@ -0,0 +1,21 @@
+## Módulo com funções variadas
+
+def mensagem():
+    print('Cristiano Ronaldo é o maior jogador de futebol de todos os tempos\n')
+
+def fatorial(numero):
+    if numero < 0:
+        return 'Digite um valor maior ou igual a zero'
+    else:
+        if numero ==0 or numero ==1:
+            return 1
+        else:
+            return numero * fatorial(numero - 1)
+
+def fibo(n):
+    resultado = [ ]
+    a, b = 0, 1
+    while b <= n:
+        resultado.append(b)
+        a, b = b, a+b
+    return resultado
